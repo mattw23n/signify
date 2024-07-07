1e65088917ebcf7e757ce8a42071584f939018b3
Matt <matteo.woenardi@gmail.com>
2024-07-07 17:48:16 +0800

    added get requests and connect frontend with backend

--- backend/django/signify/urls.py
+++ backend/django/signify/urls.py
@@ -1,6 +1,20 @@
 from django.urls import path
 from .views import VideoUploadView
+from .views import get_caption
+import logging
+
+logger= logging.getLogger(__name__)
+
+def log_path(request):
+    logger.info(f"Request path: {request.path}")
+    return get_caption(request)
 
 urlpatterns = [
     path('upload/', VideoUploadView.as_view(), name='video-upload'),
+    path('caption/', log_path, name='get_caption'),  # Add logging to the URL pattern
 ]
+
+# urlpatterns = [
+#     path('upload/', VideoUploadView.as_view(), name='video-upload'),
+#     path('caption/', get_caption, name='get_caption'),
+# ]
